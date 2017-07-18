3b0b40424e9faee2b27088bfc0d9c03cbc2ddc7e
Alan Yu <yupliu@users.noreply.github.com>
2017-07-18 12:59:47 -0400

    Start

---
+++ Shinystructure/Shinystructure/script.R
@@ -0,0 +1,25 @@
+#prepare packages for chemical data processing
+if (!require("rJava", quietly = TRUE))
+    install.packages("rJava")
+library(rJava)
+if (!require("rcdk", quietly = TRUE))
+    install.packages("rcdk")
+library(rcdk)
+if (!require("fingerprint", quietly = TRUE))
+    install.packages("fingerprint")
+library(fingerprint)
+if (!require("ChemmineR", quietly = TRUE)) {
+    source("https://bioconductor.org/biocLite.R")
+    biocLite("ChemmineR")
+}
+library(ChemmineR)
+
+#Load shiny and ggplot2
+if (!require("shiny", quietly = TRUE))
+    install.packages("shiny")
+library(shiny)
+if (!require("ggplot2", quietly = TRUE))
+    install.packages("ggplot2")
+library(ggplot2)
+
+
